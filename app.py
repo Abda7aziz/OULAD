1a98e63b2715be31560f429759327bb1de47ca97
abda7aziz <abda7aziz@gmail.com>
2023-07-25 02:19:31 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,297 @@
+import dash
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+import pandas as pd
+import geopandas as gpd
+import plotly.express as px
+import plotly.graph_objects as go
+import json
+import pyproj
+
+# Import the layout from layout.py
+from layout import layout
+
+studentInfo_df = pd.read_csv('OULAD/studentInfo_df.csv')
+student_df = pd.read_csv('OULAD/student_df.csv')
+evaluation_df = pd.read_csv('OULAD/evaluation.csv')
+
+
+gdf = gpd.read_file('NUTS_Level_1_January_2018_GCB_in_the_United_Kingdom_2022_4034932022524175836')
+gdf.to_crs(pyproj.CRS.from_epsg(4326), inplace=True)
+
+mapping_dict = {
+    'North East (England)': 'North Region',
+    'North West (England)': 'North Western Region',
+    'Yorkshire and The Humber': 'Yorkshire Region',
+    'East Midlands (England)': 'East Midlands Region',
+    'West Midlands (England)': 'West Midlands Region',
+    'East of England': 'East Anglian Region',
+    'London': 'London Region',
+    'South East (England)': 'South East Region',
+    'South West (England)': 'South West Region',
+    'Wales': 'Wales',
+    'Scotland': 'Scotland',
+    'Northern Ireland': 'Ireland'
+}
+gdf['nuts118nm'] = gdf['nuts118nm'].replace(mapping_dict)
+
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
+
+app.layout = layout
+
+# Create callbacks for any interactive components or visualizations
+# For example, if you want to update the choropleth map based on some input, create a callback here
+@app.callback(
+    Output('mStudents', 'children'),
+    Output('fStudents', 'children'),
+    Input('moduleDropdown', 'value'),
+    Input('xAxisDropDown','value'),
+    Input('barChart', 'selectedData'),
+    Input('choroplethMap','selectedData'))
+def sCards(module,x,bar,map):
+    df = studentInfo_df.copy()
+    if module != None:
+        df = df[(df['code_module']==module.split('-')[0])&(df['code_presentation']==module.split('-')[1])]
+    if bar != None:
+        if len(bar['points']) == 1:
+            df = df[df[x]==bar['points'][0]['x']]
+        elif len(bar['points']) > 1:
+            df = df[df[x].isin([bar['points'][i]['x'] for i in range(len(bar['points']))])]
+    if map != None:
+        if len(map['points']) == 1:
+            df = df[df['region']==map['points'][0]['hovertext']]
+        elif len(map['points']) > 1:
+            df = df[df['region'].isin([map['points'][i]['hovertext'] for i in range(len(map['points']))])]
+
+
+    df = df.groupby('gender')['id_student'].count().reset_index()
+    return f"{df[df['gender'] == 'M']['id_student'].values[0]:,}",f"{df[df['gender'] == 'F']['id_student'].values[0]:,}"
+
+@app.callback(
+    Output('table', 'columns'),
+    Output('table', 'data'),
+    Input('moduleDropdown', 'value'),
+    Input('xAxisDropDown','value'),
+    Input('barChart', 'selectedData'),
+    Input('choroplethMap','selectedData'))
+def table(module,x,bar,map):
+    df = evaluation_df.copy()
+    if module != None:
+        df = df[(df['code_module']==module.split('-')[0])&(df['code_presentation']==module.split('-')[1])]
+    if bar != None:
+        if len(bar['points']) == 1:
+            df = df[df[x]==bar['points'][0]['x']]
+        elif len(bar['points']) > 1:
+            df = df[df[x].isin([bar['points'][i]['x'] for i in range(len(bar['points']))])]
+    if map != None:
+        if len(map['points']) == 1:
+            df = df[df['region']==map['points'][0]['hovertext']]
+        elif len(map['points']) > 1:
+            df = df[df['region'].isin([map['points'][i]['hovertext'] for i in range(len(map['points']))])]
+        
+    df.drop(columns=['code_module','code_presentation'],inplace=True)
+    df = df[['id_student','region','module','final_result','score','sum_click']]
+    columns=[{'name': i, 'id': i} for i in df.columns.tolist()]
+    data=df.to_dict('records')  
+    
+    return columns,data
+
+
+@app.callback(
+    Output('barChart','figure'),
+    Input('moduleDropdown', 'value'),
+    Input('xAxisDropDown','value'),
+    Input('yAxisDropDown','value'),
+    Input('choroplethMap','selectedData'))
+def barChart(module,x,y,map):
+    df = studentInfo_df.copy()
+    if module != None:
+        df = df[(df['code_module']==module.split('-')[0])&(df['code_presentation']==module.split('-')[1])]
+    if map != None:
+        if len(map['points']) == 1:
+            df = df[df['region']==map['points'][0]['hovertext']]
+        elif len(map['points']) > 1:
+            df = df[df['region'].isin([map['points'][i]['hovertext'] for i in range(len(map['points']))])]
+        
+    education_order = [
+    "No Formal Qualification",
+    "Lower Than A Level",
+    "A Level or Equivalent",
+    "HE Qualification",
+    "Post Graduate Qualification"]
+    df['highest_education'] = pd.Categorical(df['highest_education'], categories=education_order, ordered=True)
+    grouped = df.groupby([x,y])['id_student'].count().reset_index()
+    fig = px.histogram(grouped,x=x, y='id_student',color=y)
+    fig.update_layout(
+        xaxis_title=x.replace('_',' ').capitalize(),
+        yaxis_title="Frequency",
+        barmode='group',
+        template='plotly_dark',
+        margin=dict(l=0, r=0, t=0, b=0),
+        )
+    return fig
+
+
+
+@app.callback(
+    Output('scatterPlot', 'figure'),
+    Input('moduleDropdown', 'value'),
+    Input('xAxisDropDown','value'),
+    Input('barChart', 'selectedData'),
+    Input('choroplethMap','selectedData'))
+def sCards(module,x,bar,map):
+    df = evaluation_df.copy()
+    if module != None:
+        df = df[df['module']==module]
+        print(df.head())
+    if bar != None:
+        if len(bar['points']) == 1:
+            df = df[df[x]==bar['points'][0]['x']]
+        elif len(bar['points']) > 1:
+            df = df[df[x].isin([bar['points'][i]['x'] for i in range(len(bar['points']))])]
+    if map != None:
+        if len(map['points']) == 1:
+            df = df[df['region']==map['points'][0]['hovertext']]
+        elif len(map['points']) > 1:
+            df = df[df['region'].isin([map['points'][i]['hovertext'] for i in range(len(map['points']))])]
+    fig = scatter_plot = px.scatter(
+        df,
+        x='sum_click',
+        y='score',
+        template='plotly_dark',
+        color='final_result',
+        category_orders={'final_result': ['Distinction','Pass','Withdrawn','Fail']},
+        title='Number of Clicks vs. Final Assessment Scores by Module',
+        labels={'sum_click': 'Number of Clicks', 'score': 'Final Assessment Scores'}
+    )
+    fig.update_layout(
+                    xaxis=dict(title='Number of Clicks'),
+                    yaxis=dict(title='Assessment Score'),
+                    showlegend=True,
+                    template='plotly_dark',
+                    margin=dict(l=0, r=0, b=0),
+                    legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1))
+    return fig
+
+@app.callback(
+    Output('areaPlot', 'figure'),
+    Input('moduleDropdown', 'value'),
+    Input('xAxisDropDown','value'),
+    Input('barChart', 'selectedData'),
+    Input('choroplethMap','selectedData'))
+def sCards(module,x,bar,map):
+    df = studentInfo_df.copy()
+    if module != None:
+        df = df[(df['code_module']==module.split('-')[0])&(df['code_presentation']==module.split('-')[1])]
+    if bar != None:
+        if len(bar['points']) == 1:
+            df = df[df[x]==bar['points'][0]['x']]
+        elif len(bar['points']) > 1:
+            df = df[df[x].isin([bar['points'][i]['x'] for i in range(len(bar['points']))])]
+    if map != None:
+        if len(map['points']) == 1:
+            df = df[df['region']==map['points'][0]['hovertext']]
+        elif len(map['points']) > 1:
+            df = df[df['region'].isin([map['points'][i]['hovertext'] for i in range(len(map['points']))])]
+    df = df.groupby(['age_band', 'final_result'])['id_student'].count().reset_index()
+    desired_order = ['Withdrawn','Fail','Pass','Distinction']
+    df['final_result'] = pd.Categorical(df['final_result'], categories=desired_order, ordered=True)
+    df = df.sort_values(by=['age_band', 'final_result'])
+    fig = go.Figure()
+
+    for result in df['final_result'].unique():
+        result_data = df[df['final_result'] == result]
+        fig.add_trace(go.Scatter(
+            x=result_data['age_band'],
+            y=result_data['id_student'],  # Convert to percentage
+            mode='lines',
+            stackgroup='one',
+            name=result,
+            groupnorm='percent'
+            ))
+    fig.update_layout(
+                    xaxis=dict(title='Age Band'),
+                    yaxis=dict(title='Percentage of Students'),
+                    showlegend=True,
+                    template='plotly_dark',
+                    margin=dict(l=0, r=0, t=0, b=0),
+                    title='Distribution of Final Results across Age Bands',
+                    legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1))
+    return fig
+
+
+
+
+
+
+
+@app.callback(
+    Output('choroplethMap', 'figure'),
+    Input('moduleDropdown', 'value'),
+    Input('xAxisDropDown','value'),
+    Input('barChart', 'selectedData'))
+def choroplethMap(module,x,bar):
+    df = studentInfo_df.copy()
+    if module != None:
+        df = df[(df['code_module']==module.split('-')[0])&(df['code_presentation']==module.split('-')[1])]
+    if bar != None:
+        if len(bar['points']) == 1:
+            df = df[df[x]==bar['points'][0]['x']]
+        elif len(bar['points']) > 1:
+            df = df[df[x].isin([bar['points'][i]['x'] for i in range(len(bar['points']))])]
+            
+    df = gdf.merge(df.groupby('region')['id_student'].count().reset_index(),left_on='nuts118nm',right_on='region',how='right')
+    data = [go.Choroplethmapbox(
+    geojson=json.loads(df.geometry.to_json()),
+    locations=df.index,
+    hovertext=df['region'], 
+    z=df['id_student'], 
+    colorscale='teal',
+    colorbar_title='Students')]
+    data.append(go.Scattergeo(locations=df.index, text=df['id_student'], mode="text"))
+
+    layout=go.Layout(mapbox_style="carto-darkmatter", mapbox_center = {"lat": 55.5, "lon": -2.5},
+                  mapbox_zoom=4.8,template='plotly_dark',margin=dict(l=0, r=0, t=20, b=0),  
+    uniformtext=dict(minsize=8, mode='hide'),geo={'fitbounds':'locations','visible':False},
+
+    )
+
+    return go.Figure(data=data,layout=layout)
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
+# @app.callback(
+#     Output('boxPlot', 'figure'),
+#     Input('moduleDropdown', 'value'),
+#     Input('barChart', 'selectedData'),
+#     Input('choroplethMap','clickData'))
+# def sCards(module,bar,map):
+#     students = evaluation_df.copy()
+#     if bar != None:
+#         pass
+#     if map != None:
+#         pass
+#     if module != None:
+#         students = students[(students['code_module']==module.split('-')[0])&(students['code_presentation']==module.split('-')[1])]
+#     fig = box_plot = px.box(
+#         students,
+#         x='age_band',
+#         y='sum_click',
+#         # range_y=[0,10000],
+#         category_orders={'age_band': ['0-35', '35-55', '55<=']},
+#         template='plotly_dark',
+#         labels={'score': 'Final Assessment Scores', 'sum_click': 'Number of Clicks'}
+#     )
+#     fig.update_layout(
+#                     xaxis=dict(title='Age Band'),
+#                     yaxis=dict(title='Number of Clicks'),
+#                     showlegend=True,
+#                     template='plotly_dark',
+#                     margin=dict(l=0, r=0, b=0),
+#                     # title='F'
+#                     legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1))
+#     return fig
---
+++ layout.py
@@ -0,0 +1,71 @@
+from dash import html,dcc,dash_table
+import dash_bootstrap_components as dbc
+
+header = html.Div(
+    [
+        html.H1("Student Data Analysis", className="display-3"),
+        dbc.Row([
+        dbc.Col(html.P("Open University Learning Analytics Dataset (OULAD)", className="lead"),width=3,style={'display': 'flex','align-items': 'left'}),
+        dbc.Col(dcc.Dropdown(placeholder='All Modules',id='moduleDropdown',
+            options=[{'label':i,'value':i}
+                    for i in ['AAA-2013J','AAA-2014J','BBB-2013B','BBB-2013J','BBB-2014B','BBB-2014J','CCC-2014B',
+                                'CCC-2014J','DDD-2013B','DDD-2013J','DDD-2014B','DDD-2014J','EEE-2013J','EEE-2014B',
+                                'EEE-2014J','FFF-2013B','FFF-2013J','FFF-2014B','FFF-2014J','GGG-2013J','GGG-2014B','GGG-2014J']],
+                    style={'width':'100%'}),width=2,style={'display': 'flex','align-items': 'left'})
+        ]),
+        html.Div(id='dummy',hidden=True)
+    ],
+className='mb-3')
+# charts
+barChart = dcc.Loading(dcc.Graph(id='barChart'))
+choroplethMap = dcc.Loading(dcc.Graph(id='choroplethMap',style={'height':'850px'}))
+boxPlot = dcc.Loading(dcc.Graph(id='boxPlot'))
+scatterPlot = dcc.Loading(dcc.Graph(id='scatterPlot'))
+areaPlot = dcc.Loading(dcc.Graph(id='areaPlot',style={'height':'353px'}))
+
+
+row1 = dbc.Row([
+    dbc.Col([
+        dbc.Row([
+        dbc.Col(dbc.Card([html.H4('Male Students',className='blue-c'),dcc.Loading(html.H2(id='mStudents',style={'display': 'flex','justify-content':'center'}))],className='card')),
+        dbc.Col(dbc.Card([html.H4('Female Students',className='red-c'),dcc.Loading(html.H2(id='fStudents',style={'display': 'flex','justify-content':'center'}))],className='card'))
+        ],className='mb-3'),
+        dbc.Row(dbc.Col(dbc.Card(dcc.Loading(dash_table.DataTable(id='table',page_size=9,
+                                style_header={'backgroundColor': 'rgb(30, 30, 30)','color': 'white'},
+                                style_data={'backgroundColor': 'rgb(20, 20, 20)','color': 'white'},
+                                css=[{ 'selector': '.current-page', 'rule': 'background-color: white;'}])),
+                                 className='card')))
+        ],width=5),
+    
+    
+    dbc.Col(dbc.Card([
+        dbc.Row([
+            dbc.Col([
+                html.P('X Axis',className='m-0'),
+                dcc.Dropdown(id='xAxisDropDown',value='imd_band',clearable=False,
+                            options=[{'label':i.replace('_',' ').capitalize(),'value':i} for i in ['age_band','imd_band','gender','highest_education']])
+                ],width=3),
+            dbc.Col([
+                html.P('Y Axis',className='m-0'),
+                dcc.Dropdown(id='yAxisDropDown',value='highest_education',clearable=False,
+                            options=[{'label':i.replace('_',' ').capitalize(),'value':i} for i in ['age_band','imd_band','gender','highest_education']])
+                ],width=3),
+            dbc.Col(width=6)],className='mx-3'),
+            barChart
+        ],className='card'),
+    width=7)],className='mb-4')
+
+row2 = dbc.Row([
+        dbc.Col([dbc.Card(scatterPlot,className='card mb-3'),
+                 dbc.Card(areaPlot,className='card'),
+                 ],width=8),
+        dbc.Col(dbc.Card(choroplethMap,className='card',style={'height':'100%'}),width=4)
+    ])
+
+layout = dbc.Container(
+    [
+        header,
+        row1,
+        row2
+    ]
+)
